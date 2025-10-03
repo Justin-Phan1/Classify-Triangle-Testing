ef364e916abfa17abdc01c03e91da3d2bb8acbaf
JPhanGT3 <148407787+JPhanGT3@users.noreply.github.com>
2025-10-02 23:25:27 -0400

    Refactor code to improve code coverage and static code analysis

--- classify_triangle.py
+++ classify_triangle.py
@@ -1,46 +1,63 @@
+'''
+SSW 567 Classify Triangle Testing Class
+@author: Justin Phan
+Pledge: I pledge my honor that I have abided by the Stevens Honor System
+'''
 class ClassifyTriangle:
-    
+    '''
+    Class for triangle classifcation
+    '''
     def __init__(self, s1, s2, s3):
+        '''
+        Triangle constructor
+        '''
         self.a = s1
         self.b = s2
         self.c = s3
-    
     def right_triangle(self):
+        '''
+        This method checks if a triangle is a right triangle
+        '''
         if self.a <= 0 or self.b <= 0 or self.c <= 0:
             return False
-        else: 
-            return round(((self.a ** 2) + (self.b ** 2)), 2) == round((self.c ** 2), 2)
-
+        return round(((self.a ** 2) + (self.b ** 2)), 2) == round((self.c ** 2), 2)
     def scalene_triangle(self):
+        '''
+        This method checks if a triangle is a scalene triangle
+        '''
         if self.a <= 0 or self.b <= 0 or self.c <= 0:
             return False
-        else:
-            return round(((self.a) != (self.b)), 2) and round(((self.b) != (self.c)), 2) and round(((self.a) != (self.c)), 2) # condition a != c
-
+        return (round(((self.a) != (self.b)), 2) and round(((self.b) != (self.c)), 2) and
+                round(((self.a) != (self.c)), 2))# condition a != c
     def equilateral_triangle(self):
+        '''
+        This method checks if a triangle is an equilateral triangle
+        '''
         if self.a <= 0 or self.b <= 0 or self.c <= 0:
             return False
-        else:
-            return round(((self.a) == round(self.b)), 2) == round((self.c), 2)
-
+        return round(((self.a) == round(self.b)), 2) == round((self.c), 2)
     def isoceles_triangle(self):
+        '''
+        This method checks if a triangle is a isoceles triangle
+        '''
         if self.a <= 0 or self.b <= 0 or self.c <= 0:
             return False
-        else:
-            return round(((self.a) == (self.b)), 2) or round(((self.a) == (self.c)), 2) or round(((self.b) == (self.c)), 2)
-    
-    # idk
+        return (round(((self.a) == (self.b)), 2) or round(((self.a) == (self.c)), 2)
+                or round(((self.b) == (self.c)), 2))
     def scalene_right_triangle(self):
+        '''
+        This method checks if a triangle is a scalene right triangle
+        '''
         if self.a <= 0 or self.b <= 0 or self.c <= 0:
             return False
-        else: 
-            return round(((self.a) != (self.b)), 2) and round(((self.b) != (self.c)), 2) and round(((self.a) != (self.c)), 2) and round(((self.a ** 2) + (self.b ** 2)), 2) == round((self.c ** 2), 2)
-    
-    # idk
+        return (round(((self.a) != (self.b)), 2) and round(((self.b) != (self.c)), 2)
+                and round(((self.a) != (self.c)), 2)
+                and round(((self.a ** 2) + (self.b ** 2)), 2) == round((self.c ** 2), 2))
     def isoceles_right_triangle(self):
+        '''
+        This method checks if a triangle is an isoceles right triangle
+        '''
         if self.a <= 0 or self.b <= 0 or self.c <= 0:
             return False
-        else:
-            return round(((self.a) == (self.b)), 2) or round(((self.b) == (self.c)), 2) or round(((self.a) == (self.c)), 2) and round(((self.a ** 2) + (self.b ** 2)), 2) == round((self.c ** 2), 2)
-    
-   
+        return (self.a == self.b or self.b == self.c or self.a == self.c) and \
+           round(self.a ** 2 + self.b ** 2, 2) == round(self.c ** 2, 2)
--- test_classify_triangle.py
+++ test_classify_triangle.py
@@ -1,4 +1,8 @@
-import pytest
+'''
+SSW 567 Classify Triangle Triangle Test Module
+@author: Justin Phan
+Pledge: I pledge my honor that I have abided by the Stevens Honor System
+'''
 import math
 from classify_triangle import ClassifyTriangle
 
@@ -8,7 +12,6 @@ def test_init():
     assert triangle.a == 3
     assert triangle.b == 4
     assert triangle.c == 5
-
 def test_right_triangle():
     """test right triangle detection"""
     triangle_1 = ClassifyTriangle(3,4,5) # right triangle
@@ -19,25 +22,24 @@ def test_right_triangle():
     assert not triangle_2.right_triangle()
     assert not triangle_3.right_triangle()
     assert triangle_4.right_triangle()
-
 def test_scalene_right_triangle():
     """test scalene right triangle detection"""
     triangle_1 = ClassifyTriangle(3,4,5) # scalene right triangle
     triangle_2 = ClassifyTriangle(3,3,3) # equilateral triangle, not right
     triangle_3 = ClassifyTriangle(-3,-4,-5) # scalene right triangle but numbers are negative
-    assert triangle_1.right_triangle()
-    assert not triangle_2.right_triangle()
-    assert not triangle_3.right_triangle() 
-
+    assert triangle_1.scalene_right_triangle()
+    assert not triangle_2.scalene_right_triangle()
+    assert not triangle_3.scalene_right_triangle()
 def test_isoceles_right_triangle():
     """test isoceles right triangle detection"""
     triangle_1 = ClassifyTriangle(1,1,math.sqrt(2)) # isoceles right triangle
     triangle_2 = ClassifyTriangle(3,3,3) # equilateral triangle, not right
-    triangle_3 = ClassifyTriangle(-1,-1,math.sqrt(2)) # isoceles right triangle but numbers are negative
-    assert triangle_1.right_triangle()
-    assert not triangle_2.right_triangle()
-    assert not triangle_3.right_triangle() 
-
+    triangle_3 = ClassifyTriangle(-1,-1,math.sqrt(2))
+    triangle_4 = ClassifyTriangle(0,0,0) # isoceles right triangle with negative values
+    assert triangle_1.isoceles_right_triangle()
+    assert not triangle_2.isoceles_right_triangle()
+    assert not triangle_3.isoceles_right_triangle()
+    assert not triangle_4.isoceles_right_triangle()
 def test_scalene_triangle():
     """test scalene triangle detection"""
     triangle_1 = ClassifyTriangle(1,7,9) # all three sides are different
@@ -46,21 +48,19 @@ def test_scalene_triangle():
     assert triangle_1.scalene_triangle()
     assert not triangle_2.scalene_triangle()
     assert not triangle_3.scalene_triangle()
-
 def test_equilateral_triangle():
     """test equilateral triangle detection"""
     triangle_1 = ClassifyTriangle(1,1,1) # all three sides are the same
-    triangle_2 = ClassifyTriangle(1,7,9) # scalene triangle, not equilateral 
-    triangle_3 = ClassifyTriangle(0,0,0) # side lengths are all equal but zero 
+    triangle_2 = ClassifyTriangle(1,7,9) # scalene triangle, not equilateral
+    triangle_3 = ClassifyTriangle(0,0,0) # side lengths are all equal but zero
     assert triangle_1.equilateral_triangle()
     assert not triangle_2.equilateral_triangle()
     assert not triangle_3.equilateral_triangle()
-
 def test_isoceles_triangle():
     """test isoceles triangle detection"""
     triangle_1 = ClassifyTriangle(1,1,9) # two sides are the same
     triangle_2 = ClassifyTriangle(1,7,9) # scalene triangle, not isoceles
-    triangle_3 = ClassifyTriangle(-1,-1,-9) # isoceles but negative numbers 
+    triangle_3 = ClassifyTriangle(-1,-1,-9) # isoceles but negative numbers
     assert triangle_1.isoceles_triangle()
     assert not triangle_2.isoceles_triangle()
     assert not triangle_3.isoceles_triangle()
